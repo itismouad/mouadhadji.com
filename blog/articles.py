708c6f6171a64346e520dc15856df501e18a6b4c
Mouad Hadji <mhadji@teslamotors.com>
2017-07-30 15:43:50 -0700

    Initial Commit

---
+++ blog/articles.py
@@ -0,0 +1,9 @@
+articles_data = [
+    {
+        'slug': 'internship-post',
+        'year': 2014,
+        'month': 8,
+        'title': 'Medium Blog Post about my Tesla internship',
+        'description': 'My first blog post ever.'
+    }
+]
---
+++ blog/utils.py
@@ -0,0 +1,27 @@
+from .articles import articles_data
+
+months = {
+    1: 'january',
+    2: 'februrary',
+    3: 'march',
+    4: 'april',
+    5: 'may',
+    6: 'june',
+    7: 'july',
+    8: 'august',
+    9: 'september',
+    10: 'october',
+    11: 'november',
+    12: 'december'
+}
+
+
+def get_title_from_slug(slug):
+    """
+    From a slug, find the right article, return it's title
+    """
+    for article in articles_data:
+        if article['slug'] == slug:
+            return article['title']
+
+    return False
