cfb27b4012e795b78512278eb4bc4e6a0ed9f0c4
Mouad Hadji <mhadji@tesla.com>
2018-07-19 14:39:36 -0700

    Adding opinion piece about the French team and the WC2018 scandal

--- blog/articles.py
+++ blog/articles.py
@@ -1,5 +1,12 @@
 articles_data = [
     {
+        'slug': 'french-team',
+        'year': 2018,
+        'month': 7,
+        'title': 'The French Team Looks Exactly Like the Gaul',
+        'description': 'Data and social-cultural stories about the French Team and my Dad.'
+    },
+    {
         'slug': 'internship-post',
         'year': 2014,
         'month': 8,
